d5756719d548fada1d77157dd843476519f720ea
saebom <sae221@naver.com>
2022-06-17 14:17:53 +0900

    Add files via upload

---
+++ keras/keras01.py
@@ -0,0 +1,31 @@
+#1. 데이터    
+import numpy as np
+x = np.array([1, 2, 3])
+y = np.array([1, 2, 3])
+
+#2. 모델구성
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(4, input_dim=1))   
+model.add(Dense(10))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(2))
+model.add(Dense(1))
+          
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=1000)
+
+#4. 평가, 예측 
+loss = model.evaluate(x, y)
+print('loss : ', loss)
+
+result = model.predict([4])
+print('4의 예측값 : ', result)
+
+# loss :  2.9605946193960245e-14
+# 4의 예측값 :  [[4.000001]]
+
