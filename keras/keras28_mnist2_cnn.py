bcf6d006c8bf3c086dbb5f8da1d5673c80912c2a
saebom <sae221@naver.com>
2022-07-11 07:27:28 +0900

    수정 및 저장

--- keras/keras28_mnist2_cnn.py
+++ keras/keras28_mnist2_cnn.py
@@ -1,9 +1,7 @@
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 from tensorflow.keras.datasets import mnist
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import time
 import matplotlib.pyplot as plt
@@ -18,15 +16,15 @@ print(x_test.shape, y_test.shape)       # (10000, 28, 28) (10000,)
 
 x_train = x_train.reshape(60000, 28, 28, 1)  # (60000, 28, 28) (60000,)
 x_test = x_test.reshape(10000, 28, 28, 1)   # (10000, 28, 28) (10000,)
-print(x_train.shape)    # (60000, 28, 28)
-print(np.unique(x_train, return_counts=True))
-print(np.unique(y_train, return_counts=True))
+# print(x_train.shape)    # (60000, 28, 28)
+# print(np.unique(x_train, return_counts=True))
 
 
 
 # [실습] 
 # acc 0.98 이상
 # convolution 3개 이상 사용
+
 import pandas as pd
 # df = pd.DataFrame(y)
 y_train = pd.get_dummies(y_train)
@@ -38,27 +36,31 @@ print(y_train.shape)
 
 #2. 모델링 
 model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=(4, 4),    # 출력 : (N, 28, 28, 64) => padding = 'same'이므로
+model.add(Conv2D(filters=32, kernel_size=(4, 4),    
                  padding='same', 
                  input_shape=(28, 28, 1)))
-model.add(MaxPooling2D(2, 2))           # 출력 : (N, 14, 14, 64)
+model.add(MaxPooling2D(2, 2))           
+model.add(Dropout(0.2))
 model.add(Conv2D(64, (4, 4), 
-                 padding='valid',                   # padding = 'valid' 가 default
-                 activation='relu'))                # 출력 : (N, 3, 3, 7)   (N, 5, 5, 7)   (N, 4, 4, 7)
-model.add(MaxPooling2D(2, 2))           # 출력 : (N, 14, 14, 64)
+                 padding='valid',                  
+                 activation='relu'))                
+model.add(MaxPooling2D(2, 2))          
+model.add(Dropout(0.2))
 model.add(Conv2D(64, (4, 4), 
-                 padding='same',                   # padding = 'valid' 가 default
+                 padding='same',                  
                  activation='relu'))
+model.add(Dropout(0.2))
 model.add(Flatten())    # (N, 63)  (N, 175)
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 model.summary()
 
 
-# #3. 컴파일, 훈련
+#3. 컴파일, 훈련
 model.compile(loss = 'categorical_crossentropy', optimizer='adam', 
-              metrics=['accuracy', 'mse'])
+              metrics=['accuracy'])
 
 import datetime
 date = datetime.datetime.now()      
@@ -68,7 +70,7 @@ print(date)
 filepath = './_ModelCheckPoint/k28/'
 filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=200, mode='min',
+earlyStopping = EarlyStopping(monitor='val_loss', patience=20, mode='min',
                               restore_best_weights=True,
                               verbose=1)
 mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
@@ -76,28 +78,26 @@ mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
                       filepath="".join([filepath, '01_', date, '_', filename])
                       )
 start_time = time.time()
-hist = model.fit(x_train, y_train, epochs=10, batch_size=32,
+hist = model.fit(x_train, y_train, epochs=50, batch_size=32,
                  validation_split=0.2,
-                 callbacks=[earlyStopping],
+                 callbacks=[earlyStopping, mcp],
                  verbose=1)
 end_time = time.time() - start_time
 
 
 #4. 평가, 예측
-acc = model.evaluate(x_test, y_test)
-# acc = model.evaluate(x_test, y_test)
+loss, acc = model.evaluate(x_test, y_test)
+print('loss : ', loss)
 print('accuracy : ', acc)
 
 import tensorflow as tf
 y_predict = model.predict(x_test)
-y_predict = tf.argmax(y_predict, axis=1)    # pandas에서 사용 : get_dummies
-y_test = tf.argmax(y_test, axis=1)          # pandas에서 사용 : get_dummies
-
+y_predict = tf.argmax(y_predict, axis=1)    
+y_test = tf.argmax(y_test, axis=1)          
 
 
 
 #=========================================== 내용 정리 ======================================================#
-#  loss :  0.07398068159818649
-#  accuracy 0.9861999750137329
-# accuracy :  [0.06225131079554558, 0.9866999983787537, 0.002123833168298006]
+# loss :  0.04272077605128288
+# accuracy :  0.9915000200271606
 #==============================================================================================================#
--- keras/keras29_cifar10_cnn.py
+++ keras/keras29_cifar10_cnn.py
@@ -1,14 +1,15 @@
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 from tensorflow.keras.datasets import cifar10
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
 
+import tensorflow as tf
+tf.random.set_seed(777)
 
 #1. 데이터
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -16,3 +17,117 @@ from matplotlib import font_manager, rc
 print(x_train.shape, y_train.shape)     # (50000, 32, 32, 3) (50000, 1)
 print(x_test.shape, y_test.shape)       # (10000, 32, 32, 3) (10000, 1)
 
+# loss의 스케일 조정을 위해 0 ~ 255 -> 0 ~ 1 범위로 만들어줌
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train/255
+x_test = x_test/255
+
+mean = np.mean(x_train, axis=(0 , 1 , 2 , 3))
+std = np.std(x_train, axis=(0 , 1 , 2 , 3))
+x_train = (x_train-mean)/std
+x_test = (x_test-mean)/std
+
+x_train = x_train.reshape(50000, 32, 32, 3)
+x_test = x_test.reshape(10000, 32, 32, 3)
+print(x_train.shape)    # (50000, 32, 32, 3)
+print(np.unique(x_train, return_counts=True))
+
+# One Hot Encoding
+from tensorflow.python.keras.utils.np_utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+print(y_train.shape, y_test.shape)
+
+
+#2. 모델링 
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), # padding='same', 
+                 activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(2, 2))  
+model.add(Dropout(0.2))     
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))                
+model.add(MaxPooling2D(2, 2))  
+model.add(Dropout(0.2))     
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(2, 2))     
+model.add(Dropout(0.2))     
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))   
+model.add(MaxPooling2D(2, 2))     
+model.add(Dropout(0.2))                    
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))  
+# model.add(Dropout(0.2))   
+             
+model.add(Flatten())    
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dropout(0.2))
+model.add(Dense(10, activation='softmax'))
+model.summary()
+
+
+#3. 컴파일, 훈련
+model.compile(loss = 'categorical_crossentropy', optimizer='Nadam', 
+              metrics=['accuracy'])
+
+import datetime
+date = datetime.datetime.now()      
+date = date.strftime("%m%d_%H%M")  
+print(date)
+
+filepath = './_ModelCheckPoint/k28/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+
+earlyStopping = EarlyStopping(monitor='val_loss', patience=10, mode='min',
+                              restore_best_weights=True,
+                              verbose=1)
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
+                      save_best_only=True, 
+                      filepath="".join([filepath, '02_', date, '_', filename])
+                      )
+start_time = time.time()
+hist = model.fit(x_train, y_train, epochs=200, batch_size=128,
+                 validation_split=0.2,
+                 callbacks=[earlyStopping, mcp],
+                 verbose=1)
+end_time = time.time() - start_time
+
+
+#4. 평가, 예측
+loss, acc = model.evaluate(x_test, y_test)
+print('loss : ', loss)
+print('accuracy : ', acc)
+
+y_predict = model.predict(x_test)
+y_predict = y_predict.argmax(axis=1)      
+y_test = y_test.argmax(axis=1)            
+
+acc = accuracy_score(y_test, y_predict)
+print("=====================================================================")   
+print('acc 스코어 : ', acc)  
+
+print("=====================================================================")
+print("걸린시간 : ", end_time)
+
+
+#그래프로 비교
+font_path = 'C:\Windows\Fonts\malgun.ttf'
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+plt.figure(figsize=(9,6))
+plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
+plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
+plt.grid()
+# plt.title('loss & val_loss')    
+plt.title('로스값과 검증로스값')    
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend()   
+plt.show()
+
+
+#=============================== loss, accuracy ====================================#
+# loss :  0.6028042435646057
+# accuracy :  0.7942000031471252
+#===================================================================================#
