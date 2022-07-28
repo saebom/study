ad45d1b1f9ca2b53191889b10a9ea32493dd6ca1
saebom <saebom221@gmail.com>
2022-07-28 19:56:56 +0900

    07.28 수정 및 저장

---
+++ keras/keras54_project1_3.py
@@ -0,0 +1,31 @@
+import os
+
+#파일 이름 변경하기
+
+file_path = 'D:\study_data\_project1\img/fashion_img/00_london_total\이미지정제'
+file_names = os.listdir(file_path)
+file_names
+
+
+
+i = 20
+for name in file_names:
+    src = os.path.join(file_path, name)
+    dst = str(i) + '.jpg'
+    dst = os.path.join(file_path, dst)
+    os.rename(src, dst)
+    i += 1
+    
+
+# 파일 사이즈 줄이기
+# import glob
+# from PIL import Image
+# files = glob.glob('D:\study_data\_project1\img/00_paris_total/*')    
+
+# for f in files:
+#     title, ext = os.path.splitext(f)
+#     if ext in ['.jpg', '.png']:
+#         img = Image.open(f)
+#         img_resize = img.resize((int(img.width/2), int(img.height/2)))
+#         img_resize.save(title + ext)
+        
