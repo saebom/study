b6632c336bea457ef9d14d60cd0beb47c42f473a
saebom <sae221@naver.com>
2022-10-09 19:53:01 +0900

    Update torch16_mnist_summary.py

--- torch/torch16_mnist_summary.py
+++ torch/torch16_mnist_summary.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 
+
 USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device('cuda:0' if USE_CUDA else 'cpu')
 print('torch : ', torch.__version__, '사용 DEVICE : ', DEVICE)
