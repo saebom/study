cde95f6931d3c80a6e6898a45b6c7b47a59b07ae
saebom <saebom221@gmail.com>
2022-11-01 19:49:35 +0900

    10.31

---
+++ ml/m57_mape2.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error
+import tensorflow as tf
+import keras
+
+
+y_true = np.array([100.,2.])
+y_pred = np.array([200.,102.])
+# mae :  100.0
+# mape :  25.5
+
+y_true = np.array([100.,200.])
+y_pred = np.array([200.,300.])
+# mae :  100.0
+# mape :  0.75
+
+
+# ==================== mae ========================= #
+mae = mean_absolute_error(
+    y_true, y_pred
+)
+print('mae : ', mae)    
+
+
+# ==================== mape ========================= #
+mape = mean_absolute_percentage_error(
+    y_true, y_pred
+)
+print('mape : ', mape) 
+
