06a96633654a089f805e33f318592e966899a9e6
saebom <saebom221@gmail.com>
2022-11-02 18:26:44 +0900

    10.31

--- ml/m57_mape2.py
+++ ml/m57_mape2.py
@@ -9,8 +9,8 @@ y_pred = np.array([200.,102.])
 # mae :  100.0
 # mape :  25.5
 
-y_true = np.array([100.,200.])
-y_pred = np.array([200.,300.])
+# y_true = np.array([100.,200.])
+# y_pred = np.array([200.,300.])
 # mae :  100.0
 # mape :  0.75
 
